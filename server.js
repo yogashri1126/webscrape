b64623cb711134accb452b913e242702c21e9a74
yogashri1126 <ypradhan.pge@texas.edu>
2017-11-01 00:11:26 -0500

    process.env the PORT too

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.port || 3000;;
 
 // Initialize Express
 var app = express();
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
+mongoose.connect("mongodb://localhost/homework2", {
   useMongoClient: true
 });
 
